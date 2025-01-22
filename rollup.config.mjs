23cacab62928dc746ec89a51a0c650e5a4fddffa
Max Timkovich <max@timkovi.ch>
2025-01-21 17:18:10 -0800

    fix manifest

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,24 +1,21 @@
 import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
 
-const output = {
-  format: 'iife',
-  dir: 'chrome-extension',
-};
-
-const plugins = [
-  resolve(),
-  commonjs({
-    include: ['node_modules/**',]
-  }),
-];
-
+const output_dir = 'chrome-extension';
 const inputs = ['background.js', 'popup.js', 'options.js'];
 const config = inputs.map(i => {
   return {
     input: `src/${i}`,
-    output,
-    plugins
+    output: {
+      format: 'cjs',
+      dir: output_dir,
+    },
+    plugins: [
+      resolve(),
+      commonjs({
+        include: ['node_modules/**',]
+      }),
+    ],
   };
 });
 
